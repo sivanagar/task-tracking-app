6840f8666abcc65da6766cf481dab8a4323ead99
sivanagar <sivanagar@gmail.com>
2025-03-03 12:58:05 +0200

    added forms to login and signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import {React, useState} from "react";
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log({ email, password });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import {React, useState} from 'react'
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log
+    ({ email, password, userName });
+  }
+
   return (
-    <div>Signup</div>
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Username</label>
+          <input
+            type="text"
+            placeholder="Username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <br />
+        <button type="submit">Signup</button>
+      </form> 
+      
+      
+      </div>
   )
 }
 
